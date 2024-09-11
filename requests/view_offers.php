e28fd25b0a0354faf9c0b9bcb77e5c63ce304f2e
kumshubham <kumashubham@gmail.com>
2024-09-11 19:02:06 +0530

    milestone incompleted

--- requests/view_offers.php
+++ requests/view_offers.php
@@ -52,6 +52,7 @@ $count_offers = $get_offers->rowCount();
 	<link href="../styles/user_nav_styles.css" rel="stylesheet">
 	<link href="../font_awesome/css/font-awesome.css" rel="stylesheet">
 	<link href="../styles/sweat_alert.css" rel="stylesheet">
+	<link href="../styles/newly_changes_style_file.css" rel="stylesheet">
 	<script type="text/javascript" src="../js/sweat_alert.js"></script>
 	<script type="text/javascript" src="../js/jquery.min.js"></script>
 	<script src="https://checkout.stripe.com/checkout.js"></script>
@@ -80,7 +81,7 @@ $count_offers = $get_offers->rowCount();
 							<i class="fa fa-clock-o"></i> <span> Request Duration: </span><span class="text-muted"> <?= $request_delivery_time; ?> </span> <br>
 							<i class="fa fa-archive"></i> <span> Request Category: </span><span class="text-muted"> <?= $request_cat_title; ?> / <?= $request_child_title; ?> </span>
 						</p>
-					</div>  
+					</div>
 				</div>
 				<?php if ($count_offers == "0") { ?>
 					<div class="card rounded-0 mb-3">
@@ -99,8 +100,8 @@ $count_offers = $get_offers->rowCount();
 					if (!$count_reviews == 0) {
 						$rattings = array();
 						while ($row_buyer_reviews = $select_buyer_reviews->fetch()) {
-						  $buyer_rating = $row_buyer_reviews->buyer_rating;
-						  array_push($rattings, $buyer_rating);
+							$buyer_rating = $row_buyer_reviews->buyer_rating;
+							array_push($rattings, $buyer_rating);
 						}
 						$total = array_sum($rattings);
 						@$average = $total / count($rattings);
@@ -140,7 +141,7 @@ $count_offers = $get_offers->rowCount();
 									</div>
 									<div class="col-md-7">
 										<h5 class="mt-md-0 mt-2">
-											<a href="<?=$site_url?>/proposals/<?= $sender_user_name; ?>/<?= $proposal_url; ?>" class="text-success">
+											<a href="<?= $site_url ?>/proposals/<?= $sender_user_name; ?>/<?= $proposal_url; ?>" class="text-success">
 												<?= $proposal_title; ?>
 											</a>
 										</h5>
@@ -154,57 +155,126 @@ $count_offers = $get_offers->rowCount();
 									</div>
 									<div class="col-md-3 responsive-border pt-md-0 pt-3">
 										<div class="offer-seller-picture">
-											<a href="<?=$site_url?>/<?= $sender_user_name; ?>" target="_blank">
+											<a href="<?= $site_url ?>/<?= $sender_user_name; ?>" target="_blank">
 												<?php if (!empty($sender_image)) { ?>
-													<img src="<?=$site_url?>/user_images/<?= $sender_image; ?>" class="rounded-circle">
+													<img src="<?= $site_url ?>/user_images/<?= $sender_image; ?>" class="rounded-circle">
 												<?php } else { ?>
-													<img src="<?=$site_url?>/user_images/empty-image.png" class="rounded-circle">
+													<img src="<?= $site_url ?>/user_images/empty-image.png" class="rounded-circle">
 												<?php } ?>
 											</a>
 
 											<?php if ($sender_level == 2) { ?>
-												<img src="<?=$site_url?>/images/level_badge_1.png" class="level-badge">
+												<img src="<?= $site_url ?>/images/level_badge_1.png" class="level-badge">
 											<?php } elseif ($sender_level == 3) { ?>
-												<img src="<?=$site_url?>/images/level_badge_2.png" class="level-badge">
+												<img src="<?= $site_url ?>/images/level_badge_2.png" class="level-badge">
 											<?php } elseif ($sender_level == 4) { ?>
-												<img src="<?=$site_url?>/images/level_badge_3.png" class="level-badge">
+												<img src="<?= $site_url ?>/images/level_badge_3.png" class="level-badge">
 											<?php } ?>
 										</div>
 										<div class="offer-seller mb-1">
 											<p class="font-weight-bold mb-1">
-												<a href="<?=$site_url?>/<?= $sender_user_name; ?>" class="text-dark" target="_blank">
+												<a href="<?= $site_url ?>/<?= $sender_user_name; ?>" class="text-dark" target="_blank">
 													<?= $sender_user_name; ?>
 												</a>
-													<small class="text-success">
-														<?= $sender_status; ?>
+												<small class="text-success">
+													<?= $sender_status; ?>
 												</small>
 											</p>
 											<div class="star-rating text-warning">
-											<?php
-											for ($seller_i = 0; $seller_i < $average_rating; $seller_i++) {
-												echo " <i class='fa fa-star'></i> ";
-											}
-											for ($seller_i = $average_rating; $seller_i < 5; $seller_i++) {
-												echo " <i class='fa fa-star-o'></i> ";
-											}
-											?>
-											<span class="text-dark m-1"><strong>
-												<?php printf("%.1f", $average); ?></strong> (<?=$count_reviews?>)</span>
+												<?php
+												for ($seller_i = 0; $seller_i < $average_rating; $seller_i++) {
+													echo " <i class='fa fa-star'></i> ";
+												}
+												for ($seller_i = $average_rating; $seller_i < 5; $seller_i++) {
+													echo " <i class='fa fa-star-o'></i> ";
+												}
+												?>
+												<span class="text-dark m-1"><strong>
+														<?php printf("%.1f", $average); ?></strong> (<?= $count_reviews ?>)</span>
 											</div>
 											<p class="user-link">
-												<a href="<?=$site_url?>/<?= $sender_user_name; ?>" class="text-success" target="_blank"> User Profile </a>
+												<a href="<?= $site_url ?>/<?= $sender_user_name; ?>" class="text-success" target="_blank"> User Profile </a>
 											</p>
 										</div>
 										<p><small><i class="fa fa-thumbs-o-up" data-toggle="tooltip" data-placement="top" title="Recommend"></i> 0 recommendation</small></p>
-										<a href="<?=$site_url?>/conversations/message?seller_id=<?= $sender_id; ?>&offer_id=<?= $offer_id; ?>" class="btn btn-sm btn-success rounded-0">
+										<a href="<?= $site_url ?>/conversations/message?seller_id=<?= $sender_id; ?>&offer_id=<?= $offer_id; ?>" class="btn btn-sm btn-success rounded-0">
 											Contact Now
 										</a>
-										<button id="order-button-<?= $offer_id; ?>" class="btn btn-sm btn-success rounded-0">
-											Order Now
-										</button>
-										<p class="mt-2"><a class="text-danger <?=($lang_dir == "right" ? 'text-right':'')?>" href="#" data-toggle="modal" data-target="#report-modal-uni" data-content-id="<?=$request_id?>" data-content-type="view_offers" data-url="<?=$site_url?>/requests/view_offers?request_id=<?=$request_id?>"><svg viewBox="0 0 24 24" role="presentation" aria-hidden="true" focusable="false" style="height:12px;width:12px;fill:#000;margin-right:5px;"><path d="m22.39 5.8-.27-.64a207.86 207.86 0 0 0 -2.17-4.87.5.5 0 0 0 -.84-.11 7.23 7.23 0 0 1 -.41.44c-.34.34-.72.67-1.13.99-1.17.87-2.38 1.39-3.57 1.39-1.21 0-2-.13-3.31-.48l-.4-.11c-1.1-.29-1.82-.41-2.79-.41a6.35 6.35 0 0 0 -1.19.12c-.87.17-1.79.49-2.72.93-.48.23-.93.47-1.35.71l-.11.07-.17-.49a.5.5 0 1 0 -.94.33l7 20a .5.5 0 0 0 .94-.33l-2.99-8.53a21.75 21.75 0 0 1 1.77-.84c.73-.31 1.44-.56 2.1-.72.61-.16 1.16-.24 1.64-.24.87 0 1.52.11 2.54.38l.4.11c1.39.37 2.26.52 3.57.52 2.85 0 5.29-1.79 5.97-3.84a.5.5 0 0 0 0-.32c-.32-.97-.87-2.36-1.58-4.04zm-4.39 7.2c-1.21 0-2-.13-3.31-.48l-.4-.11c-1.1-.29-1.82-.41-2.79-.41-.57 0-1.2.09-1.89.27a16.01 16.01 0 0 0 -2.24.77c-.53.22-1.04.46-1.51.7l-.21.11-3.17-9.06c.08-.05.17-.1.28-.17.39-.23.82-.46 1.27-.67.86-.4 1.7-.7 2.48-.85.35-.06.68-.1.99-.1.87 0 1.52.11 2.54.38l.4.11c1.38.36 2.25.51 3.56.51 1.44 0 2.85-.6 4.18-1.6.43-.33.83-.67 1.18-1.02a227.9 227.9 0 0 1 1.85 4.18l.27.63c.67 1.57 1.17 2.86 1.49 3.79-.62 1.6-2.62 3.02-4.97 3.02z" fill-rule="evenodd"></path></svg> Report Proposal</a></p>
+										<button class="btn btn-sm btn-success rounded-0" onclick="orderNowModel()">Order Now</button>
+
+										<div id="order_now_model_btn" class="model_order_now_display_style">
+											<div class="order_now_models_inner_div">
+												<h3>Choose Payment Option <span class="close_order_now_moden_btn" onclick="orderNowModelClose()">X</span></h3>
+												<hr>
+												<div class="p-2">
+													<input type="radio" name="select_one_option" id="create_milestone" /> &nbsp; Create Milestone <br>
+												</div>
+												<div class="p-2">
+													<input type="radio" name="select_one_option" id="full_payment" /> &nbsp; Full Payment <br>
+												</div>
+												<hr>
+
+												<div class="form_milestone_div">
+													<form action="" method="post" class="milestone_form_display">
+
+														<label for="">Task</label><br>
+														<input class="col-md-12" type="text" name="" id=""><br>
+
+														<label for="">amount</label> <br>
+														<input class="col-md-12" type="number" name="" id=""><br>
+
+														<label for="">Duration</label><br>
+														<input class="col-md-12" type="date" name="" id=""><br>
+
+														<button type="submit" name="submit_milestone">Submit</button>
+													</form>
+												</div>
+
+												<button id="order-button-<?= $offer_id; ?>" class="btn btn-sm btn-success rounded-0 full_payment_btn_display" style="display:none;">
+													Full Payment
+												</button>
+
+											</div>
+										</div>
+
+
+										<p class="mt-2"><a class="text-danger <?= ($lang_dir == "right" ? 'text-right' : '') ?>" href="#" data-toggle="modal" data-target="#report-modal-uni" data-content-id="<?= $request_id ?>" data-content-type="view_offers" data-url="<?= $site_url ?>/requests/view_offers?request_id=<?= $request_id ?>"><svg viewBox="0 0 24 24" role="presentation" aria-hidden="true" focusable="false" style="height:12px;width:12px;fill:#000;margin-right:5px;">
+													<path d="m22.39 5.8-.27-.64a207.86 207.86 0 0 0 -2.17-4.87.5.5 0 0 0 -.84-.11 7.23 7.23 0 0 1 -.41.44c-.34.34-.72.67-1.13.99-1.17.87-2.38 1.39-3.57 1.39-1.21 0-2-.13-3.31-.48l-.4-.11c-1.1-.29-1.82-.41-2.79-.41a6.35 6.35 0 0 0 -1.19.12c-.87.17-1.79.49-2.72.93-.48.23-.93.47-1.35.71l-.11.07-.17-.49a.5.5 0 1 0 -.94.33l7 20a .5.5 0 0 0 .94-.33l-2.99-8.53a21.75 21.75 0 0 1 1.77-.84c.73-.31 1.44-.56 2.1-.72.61-.16 1.16-.24 1.64-.24.87 0 1.52.11 2.54.38l.4.11c1.39.37 2.26.52 3.57.52 2.85 0 5.29-1.79 5.97-3.84a.5.5 0 0 0 0-.32c-.32-.97-.87-2.36-1.58-4.04zm-4.39 7.2c-1.21 0-2-.13-3.31-.48l-.4-.11c-1.1-.29-1.82-.41-2.79-.41-.57 0-1.2.09-1.89.27a16.01 16.01 0 0 0 -2.24.77c-.53.22-1.04.46-1.51.7l-.21.11-3.17-9.06c.08-.05.17-.1.28-.17.39-.23.82-.46 1.27-.67.86-.4 1.7-.7 2.48-.85.35-.06.68-.1.99-.1.87 0 1.52.11 2.54.38l.4.11c1.38.36 2.25.51 3.56.51 1.44 0 2.85-.6 4.18-1.6.43-.33.83-.67 1.18-1.02a227.9 227.9 0 0 1 1.85 4.18l.27.63c.67 1.57 1.17 2.86 1.49 3.79-.62 1.6-2.62 3.02-4.97 3.02z" fill-rule="evenodd"></path>
+												</svg> Report Proposal</a></p>
 									</div>
 								</div>
+								<script>
+									function orderNowModel() {
+										let ordernowmodelbtn = document.getElementById("order_now_model_btn");
+										ordernowmodelbtn.style.display = "block";
+									}
+
+									function orderNowModelClose() {
+										let ordernowmodelbtn = document.getElementById("order_now_model_btn");
+										ordernowmodelbtn.style.display = "none";
+									}
+									document.addEventListener("DOMContentLoaded", function() {
+										const createMilestoneRadio = document.getElementById('create_milestone');
+										const fullPaymentRadio = document.getElementById('full_payment');
+
+										const milestoneForm = document.querySelector('.milestone_form_display');
+										const fullPaymentForm = document.querySelector('.full_payment_btn_display');
+
+										function handleDisplay() {
+											if (createMilestoneRadio.checked) {
+												milestoneForm.style.display = 'block';
+												fullPaymentForm.style.display = 'none';
+											} else if (fullPaymentRadio.checked) {
+												milestoneForm.style.display = 'none';
+												fullPaymentForm.style.display = 'block';
+											}
+										}
+
+
+										createMilestoneRadio.addEventListener('change', handleDisplay);
+										fullPaymentRadio.addEventListener('change', handleDisplay);
+									});
+								</script>
 								<script>
 									$("#order-button-<?= $offer_id; ?>").click(function() {
 										request_id = "<?= $request_id; ?>";
