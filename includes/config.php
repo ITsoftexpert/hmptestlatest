5b6ae0e835a3aab50642d73ca69db1efc1b2fd3e
Nitinkumar93993 <nitinkumar93993@gmail.com>
2024-10-23 09:35:08 +0530

    config-file-change

--- includes/config.php
+++ includes/config.php
@@ -4,12 +4,13 @@
 
 error_reporting(0);
 
+// live 
 @define('DB_HOST', 'localhost');
 @define('DB_USER', 'u674474931_hmpdemo');
 @define('DB_PASS', 'zD4zu&r2Y');
 @define('DB_NAME', 'u674474931_hmpdemo');
 
-
+// local
 // @define('DB_HOST', 'localhost');
 // @define('DB_USER', 'root');
 // @define('DB_PASS', '');
