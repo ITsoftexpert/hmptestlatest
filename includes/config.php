5a5ae3032570f27696f137296833810965c6e2af
Nitinkumar93993 <nitinkumar93993@gmail.com>
2024-12-03 14:29:34 +0530

    newchange

--- includes/config.php
+++ includes/config.php
@@ -15,7 +15,7 @@ error_reporting(0);
 // @define('DB_PASS', '');
 // @define('DB_NAME', 'hmplatest3122024');
 // new shubham side
-@define('DB_HOST', 'localhost');
+@define('DB_HOST', 'hmplatest31220');
 @define('DB_USER', 'hmplatest31220');
 @define('DB_PASS', '699G+Bs0o9&G');
 @define('DB_NAME', 'hmplatest31220');
